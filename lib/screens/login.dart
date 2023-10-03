f46464b1dba5969fdf249917b221a5df8022a86d
vernonthedev <techjaja2@gmail.com>
2023-10-03 00:42:22 -0700

    added the scan tag functionality

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/input_dialog.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -54,13 +56,80 @@ class _LoginPageState extends State<LoginPage> {
           ),
           TextButton(
               onPressed: () async {
+                //assigning values of the text fields to the variables
                 final siteID = _siteID.text;
                 final password = _password.text;
-
+                //print values to terminal
                 debugPrint("Site Id is: $siteID and Password is $password");
+                //show the guard id tag verification widget
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => VerifyGuardTag()));
               },
               child: const Text("Login"))
         ],
+      ),
+    );
+  }
+}
+
+class VerifyGuardTag extends StatefulWidget {
+  @override
+  _VerifyGuardTagState createState() => _VerifyGuardTagState();
+}
+
+class _VerifyGuardTagState extends State<VerifyGuardTag> {
+  String? userInput;
+
+  _showInputDialog(BuildContext context) async {
+    final result = await showInputDialog(context);
+    setState(() {
+      userInput = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify Guard ID Credentials'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Security Guard Name:',
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              userInput ?? 'Name Goes Here',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Guard ID Tag:',
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              userInput ?? 'No Tag Id Scanned',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showInputDialog(context);
+              },
+              child: const Text('Scan Tag'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                //verify the tag details
+              },
+              child: const Text('Verify Tag Info'),
+            ),
+          ],
+        ),
       ),
     );
   }
